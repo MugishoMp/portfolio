6097438bcdd56d0ecab8b83f6c50a8a1209b0f6d
mugisho <mugishompozi@gmail.com>
2023-07-21 02:03:52 +0200

    add profile picture to site

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = ( ) => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
                 Hello, I am a Computer Science Student based in The Netherlands!
             </Box>
 
@@ -14,7 +14,26 @@ const Page = ( ) => {
                         </Heading>
                         <p> Student (Developer \ Hobbyist \ Audiophile) </p>
                 </Box>
+                <Box 
+                    flexShrink={0} 
+                    mt={{ base: 4, md: 0 }} 
+                    ml={{ md: 6 }} 
+                    align="center"
+                >
+                    <Image 
+                        borderColor="whiteAlpha.800" 
+                        borderWidth={1.5} 
+                        borderStyle="solid" 
+                        maxWidth="100px" 
+                        display="inline-block" 
+                        borderRadius="full" 
+                        src="/images/profile/Mugisho_1_Square_Zoomed.jpg" 
+                        alt="Profile Image"
+                    />
+                </Box>
             </Box>
+
+			
         </Container>
     )
 }
