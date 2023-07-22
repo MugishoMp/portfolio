23670342ea223f18447cf64a8aa9710bf8bd14fc
mugisho <mugishompozi@gmail.com>
2023-07-22 21:04:17 +0200

    Create overlay menu links

--- components/overlayMenu.js
+++ components/overlayMenu.js
@@ -1,38 +1,32 @@
-import { Box, Link, Flex, Fade } from '@chakra-ui/react'
-import NextLink from 'next/link'
-import fontConfig from '../config/fontConfig'
+import { Box, Flex } from '@chakra-ui/react'
+import { useEffect, useState } from 'react';
+import OverlayMenuLink from './overlayMenuLink.js';
 
 const OverlayMenu = ({ isOpen, onClose }) => {
+  const [menuOpacity, setMenuOpacity] = useState(0);
+
   return (
-    <Fade in={isOpen}>
-      <Box
-        position="fixed"
-        top={0}
-        left={0}
-        width="100vw"
-        height="100vh"
-        bg="rgba(0, 0, 0, 0.9)"
-        display={isOpen ? 'flex' : 'none'}
-        alignItems="center"
-        justifyContent="center"
-        zIndex={999}
-        onClick={onClose}
-      >
-        
-        <Flex p={4} flexDirection="column" textAlign="center">
-          <Link as={NextLink} href="/" fontSize={fontConfig.sizes['3xl']}>
-            Home
-          </Link>
-          <Link as={NextLink} href="/works" fontSize={fontConfig.sizes['3xl']}>
-            Works
-          </Link>
-          <Link as={NextLink} href="/posts" fontSize={fontConfig.sizes['3xl']}>
-            Posts
-          </Link>
-        </Flex>
-      </Box>
-    </Fade>
-    
+    <Box
+      position="fixed"
+      top={0}
+      left={0}
+      width="100vw"
+      height="100vh"
+      bg="rgba(0, 0, 0, 0.9)"
+      display={isOpen ? 'flex' : 'none'} 
+      alignItems="center"
+      justifyContent="center"
+      zIndex={999}
+      onClick={onClose}
+      pointerEvents={isOpen ? 'auto' : 'none'} // Control pointer events based on isOpen
+    >
+      
+      <Flex p={4} flexDirection="column" textAlign="center">
+        <OverlayMenuLink href="/"> Home </OverlayMenuLink>
+        <OverlayMenuLink href="/works"> Works </OverlayMenuLink>
+        <OverlayMenuLink href="/repo"> Repo </OverlayMenuLink>
+      </Flex>
+    </Box>
   )
 }
 
---
+++ components/overlayMenuLink.js
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { Link, useColorModeValue, Box } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import fontConfig from '../config/fontConfig';
+import { keyframes } from '@emotion/react';
+
+const blinkAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const MenuLink = ({ href, children }) => {
+  const linkColor = useColorModeValue('whiteAlpha.600', 'whiteAlpha.900');
+  const hoverColor = useColorModeValue('whiteAlpha.900', 'whiteAlpha.600');
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <Link
+      as={NextLink}
+      href={href}
+      fontSize={fontConfig.sizes['3xl']}
+      color={isHovered ? hoverColor : linkColor}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      _hover={{
+        textDecoration: 'none',
+      }}
+    >
+      {isHovered && (
+        <Box
+          as="span"
+          animation={`${blinkAnimation} 1.2s infinite`}
+        >
+          {'>'}
+        </Box>
+      )}
+      {children}
+    </Link>
+  );
+};
+
+export default MenuLink;
