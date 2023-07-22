ed82486b0bdab06dfeae29dff25cef17aa3c657f
mugisho <mugishompozi@gmail.com>
2023-07-23 00:24:16 +0200

    overlay menu logic (WIP)

--- components/overlayMenu.js
+++ components/overlayMenu.js
@@ -1,9 +1,42 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import OverlayMenuLink from './overlayMenuLink.js';
+import { keyframes, css } from '@emotion/react';
+
+const fadeInAnimation = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 0.9;
+  }
+`;
+
+const fadeOutAnimation = keyframes`
+  from {
+    opacity: 0.9;
+  }
+  to {
+    opacity: 0;
+  }
+`;
 
 const OverlayMenu = ({ isOpen, onClose }) => {
-  const [menuOpacity, setMenuOpacity] = useState(0);
+  const [showMenu, setShowMenu] = useState(isOpen);
+
+  useEffect(() => {
+    setShowMenu(isOpen);
+  }, [isOpen]);
+
+  const handleCloseMenu = () => {
+    // Start the fade-out animation
+    setShowMenu(false);
+
+    // Wait for the fade-out animation to complete before closing the menu
+    setTimeout(() => {
+      onClose();
+    }, 300); // Adjust the timeout value to match the duration of the fade-out animation
+  };
 
   return (
     <Box
@@ -13,21 +46,23 @@ const OverlayMenu = ({ isOpen, onClose }) => {
       width="100vw"
       height="100vh"
       bg="rgba(0, 0, 0, 0.9)"
-      display={isOpen ? 'flex' : 'none'} 
+      display={showMenu ? 'flex' : 'none'}
       alignItems="center"
       justifyContent="center"
       zIndex={999}
-      onClick={onClose}
-      pointerEvents={isOpen ? 'auto' : 'none'} // Control pointer events based on isOpen
+      onClick={handleCloseMenu} // Use the custom function to handle closing after fade-out animation
+      pointerEvents={showMenu ? 'auto' : 'none'}
+      css={{
+        animation: isOpen ? `${fadeInAnimation} 0.3s` : `${fadeOutAnimation} 0.3s`,
+      }}
     >
-      
       <Flex p={4} flexDirection="column" textAlign="center">
         <OverlayMenuLink href="/"> Home </OverlayMenuLink>
         <OverlayMenuLink href="/works"> Works </OverlayMenuLink>
         <OverlayMenuLink href="/repo"> Repo </OverlayMenuLink>
       </Flex>
     </Box>
-  )
-}
+  );
+};
 
-export default OverlayMenu
+export default OverlayMenu;
