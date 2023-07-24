084891dd8ed1323005c0f331df171894d568dca3
Mugisho <mugishompozi@gmail.com>
2023-07-24 12:27:28 +0200

    simple button animation works

--- components/night-mode-button.js
+++ components/night-mode-button.js
@@ -1,22 +1,36 @@
-import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Box, IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const NightModeButton = () => {
   const { toggleColorMode } = useColorMode();
 
   return (
-    <IconButton
-      aria-label="Toggle theme"
-      colorScheme={useColorModeValue('purple', 'orange')}
-      icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-      onClick={toggleColorMode}
-      borderRadius={0}
-    //   width="100%" // Set width to 100% (of parent's height)
-      height="100%" // Set height to 100% (of parent's height)
+    <AnimatePresence mode='wait' initial={false}>
+    <Box
+      as={motion.div}
+      style={{ display: 'inline-block' }}
+      key={useColorModeValue('light', 'dark')}
+      initial={{ y: -20, opacity: 0}}
+      animate={{ y: 0, opacity: 1}}
+      exit={{ y: 20, opacity: 0}}
+      transition={{ delay: 0.0 }}
+      height="100%"
       css={{
         aspectRatio: '1/1', // Set the aspect ratio to 1:1 (square)
       }}
-    ></IconButton>
+    >
+      <IconButton
+        aria-label="Toggle theme"
+        colorScheme={useColorModeValue('purple', 'orange')}
+        icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+        onClick={toggleColorMode}
+        borderRadius={0}
+        width="100%" // Set width to 100% (of parent's height)
+        height="100%" // Set height to 100% (of parent's height)
+      ></IconButton>
+    </Box>
+    </AnimatePresence>
   );
 };
 
