5ec27832341b92897bd777ee7ce1480b5c1b2074
mugisho <mugishompozi@gmail.com>
2023-07-27 00:25:10 +0200

    fix CI pipeline

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
 import { ColorModeScript } from '@chakra-ui/react';
+// prettier-ignore
 import NextDocument, {
   Html,
   Head,
   Main,
   NextScript,
 } from 'next/document';
+
 import theme from '../lib/theme';
 
 export default class Document extends NextDocument {
