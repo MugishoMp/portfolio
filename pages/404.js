3dcabe6d441360121c22363ce86d86a8be354af4
mugisho <mugishompozi@gmail.com>
2023-08-18 13:33:14 +0200

    Add gif to 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,19 @@
-export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+import {
+  Heading,
+  Text,
+  Container,
+  Divider,
+} from '@chakra-ui/react';
+
+function NotFound() {
+  return (
+    <Container>
+      <Heading as="h1" mt={16}>Not Found</Heading>
+      <Text>The page you were looking for was not found.</Text>
+      <Divider my={6} />
+      <img src="https://media.giphy.com/media/jbEz9aLxvu3Wv98BIi/giphy.gif" alt="Page not found!" />
+    </Container>
+  );
 }
+
+export default NotFound;
