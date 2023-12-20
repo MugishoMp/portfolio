ab84005a8bfe6fedd1331a7dc2ac740250848ef0
Mugisho <mugishompozi@gmail.com>
2023-12-20 15:48:30 +0100

    fix indenting

--- pages/works/grass.js
+++ pages/works/grass.js
@@ -1,61 +1,59 @@
 import {
-    Container,
-    Badge,
-    Link,
-    List,
-    ListItem,
-  } from '@chakra-ui/react';
-  import {
-    ExternalLinkIcon,
-  } from '@chakra-ui/icons';
-  import {
-    Title,
-    WorkImage,
-    Meta,
-  } from '../../components/work';
-  import P from '../../components/paragraph';
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+} from '@chakra-ui/react';
+import {
+  ExternalLinkIcon,
+} from '@chakra-ui/icons';
+import {
+  Title,
+  WorkImage,
+  Meta,
+} from '../../components/work';
+import P from '../../components/paragraph';
 
-
-
-  function Work() {
-    return (
-      <Container>
-        <Title>
-          Grass simulation
-          &nbsp;
-          <Badge>
-            2023
-          </Badge>
-        </Title>
-        <P>
+function Work() {
+  return (
+    <Container>
+      <Title>
+        Grass simulation
+        &nbsp;
+        <Badge>
+          2023
+        </Badge>
+      </Title>
+      <P>
         A visually immersive grass simulation project that replicates dynamic 
         grass fields using advanced Worley noise and shader techniques. This 
         project demonstrates expertise in graphics programming, creating realistic, 
         interactive grassy landscapes.
-        </P>
-        <List ml={4} my={4}>
-          <ListItem>
-            <Meta>Plugin</Meta>
-            <Link href="https://github.com/MugishoMp/Computer_Graphics">
-              Grass simulation
-              <ExternalLinkIcon mx="2px" />
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Meta>Platform</Meta>
-            <span>macOS / Linux</span>
-          </ListItem>
-          <ListItem>
-            <Meta>Stack</Meta>
-            <span>C++, Opengl, GLM, SDL2</span>
-          </ListItem>
-        </List>
-  
-        <WorkImage src="/images/works/grass2.png" alt="grass" />
-      </Container>
-    );
-  }
-  
-  export default Work;
-  // export { getServerSideProps } from '../../components/chakra'
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Plugin</Meta>
+          <Link href="https://github.com/MugishoMp/Computer_Graphics">
+            Grass simulation
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>macOS / Linux</span>
+        </ListItem>
+        <ListItem>
+          <Meta>Stack</Meta>
+          <span>C++, Opengl, GLM, SDL2</span>
+        </ListItem>
+      </List>
+
+      <WorkImage src="/images/works/grass2.png" alt="grass" />
+    </Container>
+  );
+}
+
+export default Work;
+// export { getServerSideProps } from '../../components/chakra'
   
